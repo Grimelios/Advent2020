3f32d0cc2c2463b66e1c6541da7d03686bf26ecb
Mark Hatfield <grimelios@gmail.com>
2020-12-06 17:19:22 -0600

    Solutions 5-6

--- Advent2020/Solver5.cs
+++ Advent2020/Solver5.cs
@@ -10,12 +10,60 @@ namespace Advent2020
 	{
 		public override object SolvePart1(string[] lines)
 		{
-			throw new NotImplementedException();
+			return lines.Max(s => ComputeSeat(s));
 		}
 
 		public override object SolvePart2(string[] lines)
 		{
-			throw new NotImplementedException();
+			var seats = lines.Select(s => ComputeSeat(s)).ToList();
+			seats.Sort();
+
+			var missing = seats[0] + 1;
+
+			for (int i = 1; i < seats.Count - 1; i++)
+			{
+				if (seats[i] != missing)
+				{
+					break;
+				}
+
+				missing++;
+			}
+
+			return missing;
+		}
+		
+		private int ComputeSeat(string s)
+		{
+			var min = 0;
+			var max = 127;
+
+			for (int i = 0; i < 6; i++)
+			{
+				switch (s[i])
+				{
+					case 'F': max -= (int)Math.Ceiling((max - min) / 2f); break;
+					case 'B': min += (int)Math.Ceiling((max - min) / 2f); break;
+				}
+			}
+
+			var row = s[6] == 'F' ? min : max;
+
+			min = 0;
+			max = 7;
+
+			for (int i = 0; i < 2; i++)
+			{
+				switch (s[i + 7])
+				{
+					case 'L': max -= (int)Math.Ceiling((max - min) / 2f); break;
+					case 'R': min += (int)Math.Ceiling((max - min) / 2f); break;
+				}
+			}
+
+			var seat = s[9] == 'L' ? min : max;
+
+			return row * 8 + seat;
 		}
 	}
 }
--- Advent2020/Solver6.cs
+++ Advent2020/Solver6.cs
@@ -10,12 +10,67 @@ namespace Advent2020
 	{
 		public override object SolvePart1(string[] lines)
 		{
-			throw new NotImplementedException();
+			var counts = new List<int>();
+			var answers = new bool[26];
+
+			foreach (var s in lines)
+			{
+				if (s == "")
+				{
+					counts.Add(answers.Count(a => a));
+
+					for (int i = 0; i < 26; i++)
+					{
+						answers[i] = false;
+					}
+
+					continue;
+				}
+
+				foreach (var c in s)
+				{
+					answers[c - 'a'] = true;
+				}
+			}
+			
+			counts.Add(answers.Count(a => a));
+
+			return counts.Sum();
 		}
 
 		public override object SolvePart2(string[] lines)
 		{
-			throw new NotImplementedException();
+			var counts = new List<int>();
+			var answers = new int[26];
+			var group = 0;
+
+			foreach (var s in lines)
+			{
+				if (s == "")
+				{
+					counts.Add(answers.Count(a => a == group));
+
+					for (int i = 0; i < 26; i++)
+					{
+						answers[i] = 0;
+					}
+
+					group = 0;
+
+					continue;
+				}
+
+				group++;
+
+				foreach (var c in s)
+				{
+					answers[c - 'a']++;
+				}
+			}
+			
+			counts.Add(answers.Count(a => a == group));
+
+			return counts.Sum();
 		}
 	}
 }
